abb342b5376386ae5b56dfe1f24ad7694ab247ed
Arnold <szederjesiarnold@yahoo.com>
2016-01-09 17:05:58 +0200

    Added repository interfaces

--- src/main/java/edu/onlinetests/persistance/QuestionStatisticDAO.java
+++ src/main/java/edu/onlinetests/persistance/QuestionStatisticDAO.java
@@ -3,10 +3,13 @@ package edu.onlinetests.persistance;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+
 import edu.onlinetests.model.Category;
 import edu.onlinetests.model.Question;
 import edu.onlinetests.model.QuestionStatistic;
 
+@Repository
 public interface QuestionStatisticDAO {
 
 	void storeQuestionStatistics(List<QuestionStatistic> questionStatistics);
--- src/main/java/edu/onlinetests/persistance/TestDAO.java
+++ src/main/java/edu/onlinetests/persistance/TestDAO.java
@@ -2,10 +2,13 @@ package edu.onlinetests.persistance;
 
 import java.util.SortedSet;
 
+import org.springframework.stereotype.Repository;
+
 import edu.onlinetests.model.Category;
 import edu.onlinetests.model.TestResult;
 import edu.onlinetests.model.User;
 
+@Repository
 public interface TestDAO {
 
 	void storeTestResult(TestResult testResult);
